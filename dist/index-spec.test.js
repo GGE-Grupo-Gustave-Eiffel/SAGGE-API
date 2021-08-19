ec6b167a3be66bc5c848f98a1da419ab64c84197
Luís Tchitue <ls04af@gmail.com>
2021-08-19 07:09:39 -0300

    Fase 7 de Automação - (changed the config.yml to only allow heroku deployment once tests pass)

--- dist/index-spec.test.js
+++ dist/index-spec.test.js
@@ -6,5 +6,8 @@ describe('Bloco do teste de sanidade', () => {
     it(`este teste de sanidade está a ser colocado só para estimular um build no TravisCI`, () => {
         expect(true).toBe(true);
     });
+    it(`deve falhar e não activar o deploy deste app no heroku`, () => {
+        expect(1 + 1).toEqual(0);
+    });
 });
 //# sourceMappingURL=index-spec.test.js.map
